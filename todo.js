16f7bfef710f5d3e59d702970a9375c8e134ebe3
DeeTomPanda <thewalkerdarkknight@gmail.com>
2021-01-09 18:29:43 +0530

    Added Comments

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 //Created on 19/12/2020
-//Program For Corona Safe Felloswhip 2020...
+//Program For Corona Safe Felloswhip 2020.....
 //My first ever try with Nodejs
 
 const commands=["add","del","done","ls","report","help"];
